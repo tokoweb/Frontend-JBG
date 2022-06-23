256a713d9e64635d3a283bcd5fdd2177eace9cef
andiramdani27 <andi8337@gmail.com>
2022-06-23 21:37:55 +0700

    add page profile, penempatan dashboard member

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -65,7 +65,7 @@ var theme = {
    * Sub Menus
    * Enables multilevel dropdown
    */
-  subMenu: () => {
+  subMenu: function () {
     (function($bs) {
       const CLASS_NAME = 'has-child-dropdown-show';
       $bs.Dropdown.prototype.toggle = function(_original) {
@@ -93,14 +93,13 @@ var theme = {
   },
   /**
    * Offcanvas
-   * Enables offcanvas-nav, closes offcanvas on anchor clicks, focuses on input in search offcanvas
+   * Enables offcanvas-nav, closes offcanvas on anchor clicks
    */
-  offCanvas: () => {
+  offCanvas: function () {
     const navOffCanvasBtn = document.querySelectorAll(".offcanvas-nav-btn");
     const navOffCanvas = document.querySelector('.navbar:not(.navbar-clone) .offcanvas-nav');
     const bsOffCanvas = new bootstrap.Offcanvas(navOffCanvas, {scroll: true});
     const scrollLink = document.querySelectorAll('.onepage .navbar li a.scroll');
-    const searchOffcanvas = document.getElementById('offcanvas-search');
     navOffCanvasBtn.forEach(e => {
       e.addEventListener('click', event => {
         bsOffCanvas.show();
@@ -111,11 +110,6 @@ var theme = {
         bsOffCanvas.hide();
       })
     });
-    if(searchOffcanvas != null) {
-      searchOffcanvas.addEventListener('shown.bs.offcanvas', function () {
-        document.getElementById("search-form").focus();
-      });
-    }
   },
   /**
    * Isotope
@@ -123,7 +117,7 @@ var theme = {
    * Requires assets/js/vendor/isotope.pkgd.min.js
    * Requires assets/js/vendor/imagesloaded.pkgd.min.js
    */
-  isotope: () => {
+  isotope: function () {
     var grids = document.querySelectorAll('.grid');
     if(grids != null) {
       grids.forEach(g => {
@@ -181,7 +175,7 @@ var theme = {
    * Onepage Header Offset
    * Adds an offset value to anchor point equal to sticky header height on a onepage
    */
-  onepageHeaderOffset: () => {
+  onepageHeaderOffset: function () {
     const header_height = document.querySelector(".navbar").offsetHeight;
     const shrinked_header_height = 75;
     const sections = document.querySelectorAll(".onepage section");
@@ -200,20 +194,21 @@ var theme = {
    * Adds smooth scroll animation to links with .scroll class
    * Requires assets/js/vendor/smoothscroll.js
    */
-  anchorSmoothScroll: () => {
+  anchorSmoothScroll: function () {
     const links = document.querySelectorAll(".scroll");
     for(const link of links) {
       link.addEventListener("click", clickHandler);
     }
     function clickHandler(e) {
-      e.preventDefault();
-      this.blur();
-      const href = this.getAttribute("href");
-      const offsetTop = document.querySelector(href).offsetTop;
-      scroll({
-        top: offsetTop,
-        behavior: "smooth"
-      });
+      // e.preventDefault();
+      // this.blur();
+      // const href = this.getAttribute("href");
+      // const offsetTop = document.querySelector(href).offsetTop;
+     
+      // scroll({
+      //   top: offsetTop,
+      //   behavior: "smooth"
+      // });
     }
   },
   /**
@@ -221,7 +216,7 @@ var theme = {
    * Replaces an img element with an inline SVG so you can apply colors to your SVGs
    * Requires assets/js/vendor/svg-inject.min.js
    */
-  svgInject: () => {
+  svgInject: function () {
     SVGInject.setOptions({
       onFail: function(img, svg) {
         img.classList.remove('svg-inject');
@@ -237,7 +232,7 @@ var theme = {
    * Background Image
    * Adds a background image link via data attribute "data-image-src"
    */
-  backgroundImage: () => {
+  backgroundImage: function () {
     var bg = document.querySelectorAll(".bg-image");
     for(var i = 0; i < bg.length; i++) {
       var url = bg[i].getAttribute('data-image-src');
@@ -248,7 +243,7 @@ var theme = {
    * Background Image Mobile
    * Adds .mobile class to background images on mobile devices for styling purposes
    */
-  backgroundImageMobile: () => {
+  backgroundImageMobile: function () {
     var isMobile = (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i)) ? true : false;
     if(isMobile) {
       document.querySelectorAll(".image-wrapper").forEach(e => {
@@ -260,7 +255,7 @@ var theme = {
    * Image Hover Overlay
    * Adds span.bg inside .overlay for simpler markup and styling purposes
    */
-  imageHoverOverlay: () => {
+  imageHoverOverlay: function () {
     var overlay = document.querySelectorAll('.overlay > a, .overlay > span');
     for(var i = 0; i < overlay.length; i++) {
       var overlay_bg = document.createElement('span');
@@ -273,7 +268,7 @@ var theme = {
    * Adds parallax animation to shapes and elements
    * Requires assets/js/vendor/rellax.min.js
    */
-  rellax: () => {
+  rellax: function () {
     if(document.querySelector(".rellax") != null) {
       window.onload = function() {
         var rellax = new Rellax('.rellax', {
@@ -293,7 +288,7 @@ var theme = {
    * Enables showing elements by scrolling
    * Requires assets/js/vendor/scrollCue.min.js
    */
-  scrollCue: () => {
+  scrollCue: function () {
     scrollCue.init({
       interval: -400,
       duration: 700,
@@ -340,15 +335,13 @@ var theme = {
       var sliderAutoHeight = slider1.getAttribute('data-autoheight') === 'true';
       var sliderMargin = slider1.getAttribute('data-margin') ? slider1.getAttribute('data-margin') : 30;
       var sliderLoop = slider1.getAttribute('data-loop') === 'true';
-      var sliderCentered = slider1.getAttribute('data-centered') === 'true';
       var swiper = slider1.querySelector('.swiper:not(.swiper-thumbs)');
       var swiperTh = slider1.querySelector('.swiper-thumbs');
       var sliderTh = new Swiper(swiperTh, {
         slidesPerView: 5,
         spaceBetween: 10,
         loop: false,
-        threshold: 2,
-        slideToClickedSlide: true
+        threshold: 2
       });
       if (slider1.getAttribute('data-thumbs') === 'true') {
         var thumbsInit = sliderTh;
@@ -388,7 +381,6 @@ var theme = {
         speed: sliderSpeed,
         slidesPerView: sliderItems,
         loop: sliderLoop,
-        centeredSlides: sliderCentered,
         spaceBetween: Number(sliderMargin),
         effect: sliderEffect,
         autoHeight: sliderAutoHeight,
@@ -433,7 +425,7 @@ var theme = {
    * Enables lightbox functionality
    * Requires assets/js/vendor/glightbox.js
    */
-  lightbox: () => {
+  lightbox: function () {
     const lightbox = GLightbox({
       selector: '*[data-glightbox]',
       touchNavigation: true,
@@ -448,7 +440,7 @@ var theme = {
         css: '',
         js: '',
         config: {
-          ratio: '',
+          ratio: '16:9',
           fullscreen: {
             enabled: false,
             iosNative: false
@@ -474,7 +466,7 @@ var theme = {
    * Enables media player
    * Requires assets/js/vendor/plyr.js
    */
-  plyr: () => {
+  plyr: function () {
     var players = Plyr.setup('.player', {
       loadSprite: true,
     });
@@ -485,7 +477,7 @@ var theme = {
    * Requires assets/js/vendor/progressbar.min.js
    * Requires assets/js/vendor/noframework.waypoints.min.js
    */
-  progressBar: () => {
+  progressBar: function () {
     const pline = document.querySelectorAll(".progressbar.line");
     const pcircle = document.querySelectorAll(".progressbar.semi-circle");
     pline.forEach(e => {
@@ -543,24 +535,24 @@ var theme = {
    * Loader
    * 
    */
-  loader: () => {
-    var preloader = document.querySelector('.page-loader');
-    if(preloader != null) {
-      document.body.onload = function(){
-        setTimeout(function() {
-          if( !preloader.classList.contains('done') )
-          {
-            preloader.classList.add('done');
-          }
-        }, 1000)
-      }
-    }
-  },
+  // loader: function () {
+  //   var preloader = document.querySelector('.page-loader');
+  //   if(preloader != null) {
+  //     document.body.onload = function(){
+  //       setTimeout(function() {
+  //         if( !preloader.classList.contains('done') )
+  //         {
+  //           preloader.classList.add('done');
+  //         }
+  //       }, 1000)
+  //     }
+  //   }
+  // },
   /**
    * Page Progress
    * Shows page progress on the bottom right corner of pages
    */
-  pageProgress: () => {
+  pageProgress: function () {
     var progressWrap = document.querySelector('.progress-wrap');
     var progressPath = document.querySelector('.progress-wrap path');
     var pathLength = progressPath.getTotalLength();
@@ -599,7 +591,7 @@ var theme = {
    * Requires assets/js/vendor/counterup.min.js
    * Requires assets/js/vendor/noframework.waypoints.min.js
    */
-  counterUp: () => {
+  counterUp: function () {
     var counterUp = window.counterUp["default"];
     const counters = document.querySelectorAll(".counter");
     counters.forEach(el => {
@@ -621,7 +613,7 @@ var theme = {
    * Enables Bootstrap tooltips
    * Requires Poppers library
    */
-  bsTooltips: () => {
+  bsTooltips: function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl, {
@@ -642,7 +634,7 @@ var theme = {
    * Enables Bootstrap popovers
    * Requires Poppers library
    */
-  bsPopovers: () => {
+  bsPopovers: function () {
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
@@ -652,7 +644,7 @@ var theme = {
    * Bootstrap Modal
    * Enables Bootstrap modal popup
    */
-  bsModal: () => {
+  bsModal: function () {
     if(document.querySelector(".modal-popup") != null) {
       var myModalPopup = new bootstrap.Modal(document.querySelector('.modal-popup'));
       setTimeout(function() {
@@ -696,7 +688,7 @@ var theme = {
    * Enables custom tooltip style for image hover docs/elements/hover.html
    * Requires assets/js/vendor/itooltip.min.js
    */
-  iTooltip: () => {
+  iTooltip: function () {
     var tooltip = new iTooltip('.itooltip')
     tooltip.init({
       className: 'itooltip-inner',
@@ -710,21 +702,11 @@ var theme = {
    * Form Validation and Contact Form submit
    * Bootstrap validation - Only sends messages if form has class ".contact-form" and is validated and shows success/fail messages
    */
-  forms: () => {
+  forms: function () {
     (function() {
       "use strict";
       window.addEventListener("load", function() {
         var forms = document.querySelectorAll(".needs-validation");
-        var inputRecaptcha = document.querySelector("input[data-recaptcha]"); 
-        window.verifyRecaptchaCallback = function (response) {
-          inputRecaptcha.value = response; 
-          inputRecaptcha.dispatchEvent(new Event("change"));
-        }
-        window.expiredRecaptchaCallback = function () {
-          var inputRecaptcha = document.querySelector("input[data-recaptcha]"); 
-          inputRecaptcha.value = ""; 
-          inputRecaptcha.dispatchEvent(new Event("change"));
-        }
         var validation = Array.prototype.filter.call(forms, function(form) {
           form.addEventListener("submit", function(event) {
             if(form.checkValidity() === false) {
@@ -737,6 +719,7 @@ var theme = {
               form.classList.remove("was-validated");
               // Send message only if the form has class .contact-form
               var isContactForm = form.classList.contains('contact-form');
+              console.log(isContactForm);
               if(isContactForm) {
                 var data = new FormData(form);
                 var alertClass = 'alert-danger';
@@ -753,7 +736,6 @@ var theme = {
                   if(alertClass && txt) {
                     form.querySelector(".messages").insertAdjacentHTML('beforeend', alertBox);
                     form.reset();
-                    grecaptcha.reset();
                   }
                 }).catch((err) => {
                   console.log(err);
@@ -769,7 +751,7 @@ var theme = {
    * Password Visibility Toggle
    * Toggles password visibility in password input
    */
-  passVisibility: () => {
+  passVisibility: function () {
     let pass = document.querySelectorAll('.password-field');
     for (let i = 0; i < pass.length; i++) {
       let passInput = pass[i].querySelector('.form-control');
@@ -791,32 +773,30 @@ var theme = {
    * Pricing Switcher
    * Enables monthly/yearly switcher seen on pricing tables
    */
-  pricingSwitcher: () => {
-    if(document.querySelector(".pricing-switchers") != null) {
-      const wrapper = document.querySelectorAll(".pricing-wrapper");
-      wrapper.forEach(wrap => {
-        const switchers = wrap.querySelector(".pricing-switchers");
-        const switcher = wrap.querySelectorAll(".pricing-switcher");
-        const price = wrap.querySelectorAll(".price");
-        switchers.addEventListener("click", (e) => {
-          switcher.forEach(s => {
-            s.classList.toggle("pricing-switcher-active");
-          });
-          price.forEach(p => {
-            p.classList.remove("price-hidden");
-            p.classList.toggle("price-show");
-            p.classList.toggle("price-hide");
-          });
+  pricingSwitcher: function () {
+    const wrapper = document.querySelectorAll(".pricing-wrapper");
+    wrapper.forEach(wrap => {
+      const switchers = wrap.querySelector(".pricing-switchers");
+      const switcher = wrap.querySelectorAll(".pricing-switcher");
+      const price = wrap.querySelectorAll(".price");
+      switchers.addEventListener("click", (e) => {
+        switcher.forEach(s => {
+          s.classList.toggle("pricing-switcher-active");
+        });
+        price.forEach(p => {
+          p.classList.remove("price-hidden");
+          p.classList.toggle("price-show");
+          p.classList.toggle("price-hide");
         });
       });
-    }
+    });
   },
   /**
    * ReplaceMe.js
    * Enables text rotator
    * Requires assets/js/vendor/replaceme.min.js
    */
-  textRotator: () => {
+  textRotator: function () {
     if(document.querySelector(".rotator-zoom") != null) {
       var replace = new ReplaceMe(document.querySelector('.rotator-zoom'), {
         animation: 'animate__animated animate__zoomIn',
@@ -841,7 +821,7 @@ var theme = {
    * Enables clipboard on docs
    * Requires assets/js/vendor/clipboard.min.js
    */
-  codeSnippet: () => {
+  codeSnippet: function () {
     var btnHtml = '<button type="button" class="btn btn-sm btn-white rounded-pill btn-clipboard">Copy</button>'
     document.querySelectorAll('.code-wrapper-inner').forEach(function(element) {
       element.insertAdjacentHTML('beforebegin', btnHtml)
